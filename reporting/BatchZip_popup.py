d7cd2d7416b23c888b9e8d834451512c7e7cd98b
Chris Buurma <cbuurma@gmail.com>
2017-10-09 09:46:30 -0500

    Switched to tkinter for popup

--- reporting/BatchZip_popup.py
+++ reporting/BatchZip_popup.py
@@ -1,9 +1,8 @@
 import zipfile, os, zlib
 from tkinter import filedialog
-from PyQt4.QtGui import QFileDialog, QApplication
-app = QApplication([])
-# rootpath = filedialog.askdirectory()
-rootpath = QFileDialog.getExistingDirectory(caption='Select batch directory')
+
+rootpath = filedialog.askdirectory()
+# rootpath = QFileDialog.getExistingDirectory(caption='Select batch directory')
 # from glob import glob
 compression = zipfile.ZIP_DEFLATED
 # rootpath = os.path.abspath('')  # get the calling directory path
@@ -16,7 +15,7 @@ print ("Creating archive for batch " + batchname)
 zf = zipfile.ZipFile(batchname + ".zip", mode='w')
 
 alloutpaths = []
-ext_list = ['out','in','slurmout.txt', '.sh', '.vasp']
+ext_list = ['out', 'in', 'slurmout.txt', '.sh', '.vasp']
 for folder in os.walk(batchname):
     alloutpaths.extend([folder[0] + os.sep + file for file in os.listdir(folder[0]) if any(file.endswith(ext) for ext in ext_list)])
 
